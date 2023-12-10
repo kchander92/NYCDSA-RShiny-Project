39943a70fc02467b37a7184582f19ebdbdf07a7d
Krishnan-Chander <krishnan.chander@gmail.com>
2023-12-10 14:49:58 -0700

    Add stock image to www folder and insert image in UI sidebar

--- ui.R
+++ ui.R
@@ -1,19 +1,10 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 
 # Define UI for application that draws a histogram
 fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Housing Supply and Demand in the USA's Sun Belt"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
@@ -22,12 +13,12 @@ fluidPage(
                         "Number of bins:",
                         min = 1,
                         max = 50,
-                        value = 30)
+                        value = 30),
+            img(src='arizona_houses.jpg', width = '450px', height = '300px')
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
         )
     )
 )
