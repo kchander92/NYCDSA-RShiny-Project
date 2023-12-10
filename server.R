dcfbce363ee8c95a26e0f3eed6205c74a749fc04
Krishnan-Chander <krishnan.chander@gmail.com>
2023-12-10 15:33:07 -0700

    Create trend plot of new listings filtered by metro city selection

--- server.R
+++ server.R
@@ -1,28 +1,15 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(dplyr)
+library(ggplot2)
 
-# Define server logic required to draw a histogram
 function(input, output, session) {
 
-    output$distPlot <- renderPlot({
+    output$newListingTrend <- renderPlot({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
+        sunbelt_housing %>%
+          filter(Metro.City == input$Metro.City) %>%
+          ggplot(aes(x = Period.End, y = adjusted_average_new_listings)) +
+          geom_line() + geom_point()
     })
 
 }
--- ui.R
+++ ui.R
@@ -9,16 +9,15 @@ fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-            img(src='arizona_houses.jpg', width = '450px', height = '300px')
+          selectizeInput(inputId = 'Metro.City',
+                         label = 'Metro Area City',
+                         choices = unique(sunbelt_housing$Metro.City)),
+          img(src='arizona_houses.jpg', width = '450px', height = '300px')
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
+          plotOutput('newListingTrend')
         )
     )
 )
