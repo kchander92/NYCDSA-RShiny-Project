fa5e9779b769716c46f282593797300e7599d736
Krishnan-Chander <krishnan.chander@gmail.com>
2023-12-10 23:04:56 -0700

    Modify time trend inputs to be set by check boxes and add reactive data processing to server

--- server.R
+++ server.R
@@ -3,13 +3,21 @@ library(dplyr)
 library(ggplot2)
 
 function(input, output, session) {
+  
+    trendsByCity = reactive({
+      sunbelt_housing %>%
+        select(Period.End, Metro.City,
+               adjusted_average_new_listings, adjusted_average_new_listings_yoy,
+               adjusted_average_homes_sold, adjusted_average_homes_sold_yoy,
+               Median.Sale.Price, Median.Sale.Price.Yoy) %>%
+        filter(Metro.City %in% input$Metro.City) 
+    })
 
     output$newListingTrend <- renderPlot({
-
-        sunbelt_housing %>%
-          filter(Metro.City == input$Metro.City) %>%
-          ggplot(aes(x = Period.End, y = adjusted_average_new_listings)) +
-          geom_line() + geom_point()
+      trendsByCity() %>% 
+        ggplot(aes(x = Period.End, y=adjusted_average_new_listings)) +
+        geom_line(aes(color=Metro.City)) +
+        labs(x='Period End Date', y='New Listings', title='New Listings')
     })
 
 }
--- ui.R
+++ ui.R
@@ -9,9 +9,13 @@ fluidPage(
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-          selectizeInput(inputId = 'Metro.City',
-                         label = 'Metro Area City',
-                         choices = unique(sunbelt_housing$Metro.City)),
+#          selectizeInput(inputId = 'Metro.City',
+#                         label = 'Metro Area City',
+#                         choices = unique(sunbelt_housing$Metro.City)),
+          checkboxGroupInput(inputId = 'Metro.City',
+                             label = h3('Metro Area City'),
+                             choices = unique(sunbelt_housing$Metro.City),
+                             selected = 'Atlanta'),
           img(src='arizona_houses.jpg', width = '450px', height = '300px')
         ),
 
