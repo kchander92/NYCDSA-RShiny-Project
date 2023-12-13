79275f725fbbb7bcda835715e1b300ecf16f66ca
Krishnan-Chander <krishnan.chander@gmail.com>
2023-12-13 01:06:41 -0700

    Create new tab in sidebar menu for plotting correlations between features

--- ui.R
+++ ui.R
@@ -31,63 +31,91 @@ dashboardPage(
         
         tabItem(
           tabName = 'correlations',
-          box()
+          box(
+            fluidRow(
+              column(
+                width=6,
+                radioButtons(inputId = 'xMetric',
+                             label = 'X-Axis',
+                             choices = col_choices)
+              ),
+              column(
+                width=6,
+                radioButtons(inputId = 'yMetric',
+                             label = 'Y-Axis',
+                             choices = col_choices)
+              )
+            )
+          )
         )
       ),
       
       box(
         fluidRow(
-          column(width=3, 
-                 checkboxGroupInput(inputId = 'Metro.CityAZ',
-                                    label = 'Arizona',
-                                    choices = unique(filter(sunbelt_housing,
-                                                            Metro.State == 'AZ')$Metro.City))),
+          column(
+            width=3, 
+            checkboxGroupInput(
+              inputId = 'Metro.CityAZ',
+              label = 'Arizona',
+              choices = unique(filter(sunbelt_housing,
+                                      Metro.State == 'AZ')$Metro.City))),
           
-          column(width=3,
-                 checkboxGroupInput(inputId = 'Metro.CityGA',
-                                    label = 'Georgia',
-                                    choices = unique(filter(sunbelt_housing,
-                                                            Metro.State == 'GA')$Metro.City))),
-          column(width=3,
-                 checkboxGroupInput(inputId = 'Metro.CityLA',
-                                    label = 'Louisiana',
-                                    choices = unique(filter(sunbelt_housing,
-                                                            Metro.State == 'LA')$Metro.City))),
+          column(
+            width=3,
+            checkboxGroupInput(
+              inputId = 'Metro.CityGA',
+              label = 'Georgia',
+              choices = unique(filter(sunbelt_housing,
+                                      Metro.State == 'GA')$Metro.City))),
+          column(
+            width=3,
+            checkboxGroupInput(
+              inputId = 'Metro.CityLA',
+              label = 'Louisiana',
+              choices = unique(filter(sunbelt_housing,
+                                      Metro.State == 'LA')$Metro.City))),
           
-          column(width=3,
-                 checkboxGroupInput(inputId = 'Metro.CityNV',
-                                    label = 'Nevada',
-                                    choices = unique(filter(sunbelt_housing,
-                                                            Metro.State == 'NV')$Metro.City)))),
+          column(
+            width=3,
+            checkboxGroupInput(
+              inputId = 'Metro.CityNV',
+              label = 'Nevada',
+              choices = unique(filter(sunbelt_housing,
+                                      Metro.State == 'NV')$Metro.City)))),
         
         
         fluidRow(
-          column(width=4,
-                 checkboxGroupInput(inputId = 'Metro.CityCA',
-                                    label = 'California',
-                                    choices = unique(filter(sunbelt_housing,
-                                                            Metro.State == 'CA')$Metro.City))),
+          column(
+            width=4,
+            checkboxGroupInput(
+              inputId = 'Metro.CityCA',
+              label = 'California',
+              choices = unique(filter(sunbelt_housing,
+                                      Metro.State == 'CA')$Metro.City))),
           
-          column(width=4,
-                 checkboxGroupInput(inputId = 'Metro.CityFL',
-                                    label = 'Florida',
-                                    choices = unique(filter(sunbelt_housing,
-                                                            Metro.State == 'FL')$Metro.City))),
+          column(
+            width=4,
+            checkboxGroupInput(
+              inputId = 'Metro.CityFL',
+              label = 'Florida',
+              choices = unique(filter(sunbelt_housing,
+                                      Metro.State == 'FL')$Metro.City))),
           
-          column(width=4,
-                 checkboxGroupInput(inputId = 'Metro.CityTX',
-                                    label = 'Texas',
-                                    choices = unique(filter(sunbelt_housing,
-                                                            Metro.State == 'TX')$Metro.City)))),
+          column(
+            width=4,
+            checkboxGroupInput(
+              inputId = 'Metro.CityTX',
+              label = 'Texas',
+              choices = unique(filter(sunbelt_housing,
+                                      Metro.State == 'TX')$Metro.City)))),
         
-        
-        dateRangeInput(inputId = 'Dates',
-                       label = 'Date Range',
-                       start = min(sunbelt_housing$Period.Begin),
-                       end = max(sunbelt_housing$Period.End))
+        dateRangeInput(
+          inputId = 'Dates',
+          label = 'Date Range',
+          start = min(sunbelt_housing$Period.Begin),
+          end = max(sunbelt_housing$Period.End))
       )
     )
-    
   )
 )
 
