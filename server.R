510acf68485f3f1a0cab8d3bef2231a1be4e2af2
Krishnan-Chander <krishnan.chander@gmail.com>
2023-12-12 19:47:37 -0700

    Change UI code to run Shiny Dashboard

--- server.R
+++ server.R
@@ -1,4 +1,4 @@
-library(shiny)
+library(shinydashboard)
 library(dplyr)
 library(ggplot2)
 
--- ui.R
+++ ui.R
@@ -1,34 +1,35 @@
-library(shiny)
+library(shinydashboard)
 
-# Define UI for application that draws a histogram
-fluidPage(
+dashboardPage(
 
-    # Application title
-    titlePanel("Housing Supply and Demand in the USA's Sun Belt"),
+    dashboardHeader(title="Housing Supply and Demand in the USA's Sun Belt",
+                    titleWidth = 500),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-          checkboxGroupInput(inputId = 'Metro.City',
+    dashboardSidebar(
+      
+        sidebarUserPanel('Krishnan Chander'),
+      
+        dateRangeInput(inputId = 'Dates',
+                     label = 'Date Range',
+                     start = min(sunbelt_housing$Period.Begin),
+                     end = max(sunbelt_housing$Period.End)),
+        
+        checkboxGroupInput(inputId = 'Metro.City',
                              label = 'Metro Area City',
                              choices = unique(sunbelt_housing$Metro.City),
                              selected = 'Atlanta'),
           
-          radioButtons(inputId = 'Metric',
+        radioButtons(inputId = 'Metric',
                        label = 'Housing Metric',
-                       choices = col_choices),
+                       choices = col_choices)
           
-          dateRangeInput(inputId = 'Dates',
-                         label = 'Date Range',
-                         start = min(sunbelt_housing$Period.Begin),
-                         end = max(sunbelt_housing$Period.End)),
-          
-          img(src='arizona_houses.jpg', width = '450px', height = '300px')
-        ),
+        #img(src='arizona_houses.jpg', width = '450px', height = '300px')
+    ),
 
-        # Show a plot of the generated distribution
-        mainPanel(
+    dashboardBody(
+        box(
           plotOutput('newListingTrend')
         )
     )
 )
+
